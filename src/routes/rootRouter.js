f318b5a82ef599cbca508ec34eeecccb84b8d893
Nick Alberts <nwalberts5@gmail.com>
2024-04-29 13:36:14 -0400

    add handsome dashboard

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -2,7 +2,9 @@ import express from "express"
 import articlesRouter from "./articlesRouter.js"
 
 const rootRouter = new express.Router()
-
+rootRouter.get("/dashboard", (req, res) => {
+  res.send("look at my handsome dashboard")
+}) 
 rootRouter.use("/articles", articlesRouter)
 
 export default rootRouter
