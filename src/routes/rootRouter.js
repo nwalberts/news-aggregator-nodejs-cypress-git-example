2ae167a679ff15a3ffe46d176bad24611225a6f0
Nick Alberts <nwalberts5@gmail.com>
2024-04-29 13:34:27 -0400

    done with pretty home page

--- src/routes/rootRouter.js
+++ src/routes/rootRouter.js
@@ -2,7 +2,9 @@ import express from "express"
 import articlesRouter from "./articlesRouter.js"
 
 const rootRouter = new express.Router()
-
+rootRouter.get("/homepage", req, res => {
+  res.send("look at my pretty sandwich app")
+})
 rootRouter.use("/articles", articlesRouter)
 
 export default rootRouter
